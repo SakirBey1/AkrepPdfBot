42422fe5d44353a62f44c7abfaa58c83a15f06af
SakirBey1 <sakirhack81@gmail.com>
2021-12-02 21:12:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import os
+import telebot
 
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['hello'])
+def hello(message):
+  bot.send_message(message.chat.id, "Hello!")
+
+bot.polling()
